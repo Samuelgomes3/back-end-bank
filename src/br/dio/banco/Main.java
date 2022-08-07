2d3a4b248c056a8788c0a6ca70d196df4b93416a
Samuel <samuelgomesdev@gmail.com>
2022-08-07 02:15:00 -0300

    Commit inicial

---
+++ src/br/dio/banco/Main.java
@@ -0,0 +1,19 @@
+package br.dio.banco;
+
+public class Main {
+
+    public static void main(String[] args){
+        Cliente joao = new Cliente();
+        joao.setNome("Joao");
+
+        Conta cc = new ContaCorrente(joao);
+        Conta poupanca = new ContaPoupanca(joao);
+
+
+        cc.depositar(100);
+        cc.transferir(100, poupanca);
+
+        cc.imprimirExtrato();
+        poupanca.imprimirExtrato();
+    }
+}
